3c0465c19092f804762857cbfaaeadf0ff3e6928
francisco-arellano <113812290+francisco-arellano@users.noreply.github.com>
2025-02-16 00:00:06 -0600

    ft: Added curve plotting
    Added the plotting function for DET and ROC curve's to utils.py which is used on the AlternativeModels.ipynb

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from sklearn.metrics import (classification_report, roc_auc_score, precision_recall_curve,
                              auc, roc_curve, det_curve)
-
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 def load_data():
     """
@@ -46,3 +47,32 @@ def print_classification_report(y_true, y_pred, y_prob, label=""):
     print(f"{label} Precision-Recall AUC: {pr_auc:.4f}")
 
 
+def plot_roc_det_curve(model_name, y_val, y_pred_proba, label_name):
+  plt.figure(figsize=(28, 14))
+  palette = sns.color_palette("husl")
+
+  fpr, tpr, _ = roc_curve(y_val[label_name], y_pred_proba)
+  fpr_det, fnr_det, _ = det_curve(y_val[label_name], y_pred_proba)
+
+  plt.subplot(1, 2, 1)
+  sns.lineplot(x=fpr, y=tpr, label=f"{model_name} (AUC = {roc_auc_score(y_val[label_name], y_pred_proba):.2f})",
+                  color=palette[0])
+
+  plt.subplot(1, 2, 2)
+  sns.lineplot(x=fpr_det, y=fnr_det, label=model_name, color=palette[0])
+
+  plt.subplot(1, 2, 1)
+  plt.plot([0, 1], [0, 1], linestyle="--", color="gray", alpha=0.7)
+  plt.xlabel("False Positive Rate")
+  plt.ylabel("True Positive Rate")
+  plt.title("ROC Curve Comparison")
+  plt.legend()
+
+  plt.subplot(1, 2, 2)
+  plt.xlabel("False Positive Rate")
+  plt.ylabel("False Negative Rate")
+  plt.title("DET Curve Comparison")
+  plt.legend()
+
+  plt.tight_layout()
+  plt.show()
